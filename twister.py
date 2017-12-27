b1d8d2e52a0239b7f60ca25a93317ed83fa595de
Tim Johnsen <tjohnsen@gmail.com>
2017-12-26 23:46:09 -0500

    Add "animation" and countdown display. * animation is supposed to emulate spinning the spinner * add count down to printout in seconds * remove trailing spaces and tabs.

--- twister.py
+++ twister.py
@@ -2,35 +2,37 @@
 # -*- coding: utf-8 -*-
 #
 #  twister.py
-#  
+#
 #  Copyright 2017  <tjohnsen@gmail.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 import random, time, argparse, sys
 import tempfile
 from pygame import mixer
+from sys import stdout
 
 LIMBS = ["arm", "leg"]
 SIDES = ["right", "left"]
 COLORS= ["red", "blue", "yellow", "green"]
-WAIT = 10
-AUDIO= True
+WAIT =  10
+AUDIO = True
+ANIMATE=False
 
 def _rand(data):
     index = random.randint(0,len(data)-1)
@@ -40,7 +42,7 @@ def get_move():
     limb = _rand(LIMBS)
     side = _rand(SIDES)
     color= _rand(COLORS)
-    
+
     return ' '.join([side,limb,color])
 
 def play_move(str_move):
@@ -50,30 +52,69 @@ def play_move(str_move):
         mixer.init()
         mixer.music.load(f.name)
         mixer.music.play()
-        while mixer.music.get_busy(): # pause this script until the 
+        while mixer.music.get_busy(): # pause this script until the
             time.sleep(.1)              # audio finishes
 
-def pause(total=10):
-    from sys import stdout
+def animate(delay=10):
+    delay_width = len(str(delay))
+    increment = .1/delay
+    rest = increment
+    sides,limbs,colors=len(SIDES),len(LIMBS),len(COLORS)
+    side = random.randint(0,sides-1)
+    limb = random.randint(0,limbs-1)
+    color = random.randint(0,colors-1)
     start = time.time()
-    while time.time() - start < total:
+    x,y,z,=limb,side,color
+    duration = time.time() - start
+    while duration < delay:
+        z+=1
+        z%=colors
+        if z == color:
+            y+=1
+            y%=sides
+            if y == side:
+                x+=1
+                x%=limbs
+        time_left = "[{delay:{width}d}] ".format(\
+            delay=int(delay-duration)+1, width=delay_width)
+        move = ' '.join([SIDES[y],LIMBS[x],COLORS[z]])
+        flash_move(time_left + move,rest)
+        rest+=increment
+        duration = time.time() - start
+    return move
+
+
+def pause(delay=10):
+    delay_width = len(str(delay))
+    start = time.time()
+    duration = time.time() - start
+    while duration < delay:
         move = get_move()
-        stdout.write(move)
-        stdout.flush()
-        time.sleep(.05)
-        stdout.write('\r')
-        stdout.write(' '*len(move))
-        stdout.write('\r')
-        stdout.flush()
+        time_left = "[{delay:{width}d}] ".format(\
+            delay=int(delay-duration)+1, width=delay_width)
+        flash_move(time_left + move)
+        duration = time.time() - start
+
+def flash_move(move, rest=.05):
+    stdout.write(move)
+    stdout.flush()
+    time.sleep(rest)
+    stdout.write('\r')
+    stdout.write(' '*len(move))
+    stdout.write('\r')
 
 def main():
     while True:
         try:
-            move = get_move()
-            print(move)
+            if ANIMATE:
+                move = animate(WAIT)
+            else:
+                move = get_move()
+            print(move.upper())
             if AUDIO:
                 play_move(move)
-            pause(total=WAIT)
+            if not ANIMATE:
+                pause(delay=WAIT)
         except KeyboardInterrupt:
             exit(0)
     return 0
@@ -83,8 +124,10 @@ def parse_args():
             description='Give twister commands every 10 seconds.')
     parser.add_argument('-s', '--wait', metavar='seconds', type=int,
             help='number of seconds between each command')
-    parser.add_argument('-n', '--no-audio', action='store_true', 
+    parser.add_argument('-n', '--no-audio', action='store_true',
             help='only display commands to screen')
+    parser.add_argument('-a', '--animate', action='store_true',
+            help='animate text version of spinning')
     args = parser.parse_args()
     if args.wait:
         global WAIT
@@ -92,6 +135,9 @@ def parse_args():
     if args.no_audio:
         global AUDIO
         AUDIO=False
+    if args.animate:
+        global ANIMATE
+        ANIMATE=True
 
 if __name__ == '__main__':
     args = parse_args()
@@ -104,4 +150,3 @@ if __name__ == '__main__':
             print("To install, try: pip{} install --upgrade gTTS".format( \
             '3' if sys.version_info > (3,0) else '2'))
     main()
-
